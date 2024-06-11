c1ae90d9c986e1f3fd359ed653850bd87e3ea107
kajal-jotwani <kajaljotwani06@gmail.com>
2024-06-12 00:10:10 +0530

    done

---
+++ learning/first.py
@@ -0,0 +1,46 @@
+
+# mark = 76
+# if mark < 40:
+#     status = "Fail"
+#     letterGrade = "F"
+# else:
+#     status = "Pass"
+#     if mark < 50:
+#         letterGrade = "D"
+#     elif mark < 60:
+#         letterGrade = "C"
+#     elif mark < 75:
+#         letterGrade = "B"
+#     elif mark < 90:
+#         letterGrade = "A"
+#     else:
+#         letterGrade = "A+"
+
+# print(letterGrade)
+
+# print( "hello world")
+# print( 1j * 1j)
+
+# print(2 + 4)
+# print(12878 + 1)
+# print(7 - 3)
+# print(127 - 128)
+# print(8 * 6)
+# print(17 * 12)
+# print(2 ** 3)
+
+# a = 5
+# b = 6
+# c = -7
+
+# α = (-b + (b * b - 4 * a * c) ** 0.5)/2 * a
+# β = (-b - (b * b - 4 * a * c) ** 0.5)/2 * a
+
+# print(f"The two roots of {a}x^2 + {b}x {c} are {α:.5} and {β:.5}")
+
+import time
+
+for seconds in range(10,0,-1):
+    print(seconds)
+    time.sleep(1)
+print("happy new year")
